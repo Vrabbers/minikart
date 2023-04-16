b4326440335809e37fcfb469a0ff406ec7ca36b9
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2023-04-16 12:43:19 -0300

    center on player

--- source/main.c
+++ source/main.c
@@ -64,22 +64,8 @@ int main(void)
     }
 }
 
-void game(void)
+void random_karts(void)
 {
-    clear_shoam();
-
-    keys = ~REG_KEYINPUT;
-
-    if (keys & KEY_LEFT)
-        karts[0].theta_vel = F8_8(5.0);
-    else if (keys & KEY_RIGHT)
-        karts[0].theta_vel = F8_8(-5.0);
-    else 
-        karts[0].theta_vel = 0;
-
-    if (keys & KEY_A)
-        karts[0].vel = F24_8(5.0);
-    
     for (int i = 1; i < 8; i++)
     {
         struct kart* k = &karts[i];
@@ -96,25 +82,74 @@ void game(void)
             k->timer--;
         }
     }
+}
+
+void update_karts(void)
+{
     for (int i = 0; i < 8; i++)
     {
         struct kart* k = &karts[i];
         f24_8 dx = (lut_cos(k->theta) * k->vel) >> 8;
         f24_8 dy = (lut_sin(k->theta) * k->vel) >> 8;
-        k->x = clamp(F24_8(0.0), k->x + dx, F24_8(SCREEN_WIDTH));
-        k->y = clamp(F24_8(0.0), k->y - dy, F24_8(SCREEN_HEIGHT));
+        k->x = clamp(F24_8(-130), k->x + dx, F24_8(130));
+        k->y = clamp(F24_8(-130), k->y - dy, F24_8(130));
         k->theta += k->theta_vel;
         k->vel = (k->vel * F24_8(95/100.0)) >> 8;
+    }
+}
 
+void render_karts(void)
+{
+    struct kart me_kart = karts[0];
+    //render kart #0 (the player kart)
+    OBJATTR* me = give_shoam_slot();
+    ShoamAffinePtr affine_ptr;
+    u32 affine_ix = give_shoam_affine_slot(&affine_ptr);
+    ObjAffineSource affine_source = { F8_8(1.0), F8_8(1.0), me_kart.theta };
+    ObjAffineSet(&affine_source, affine_ptr, 1, 8);
+
+    me->attr0 = OBJ_Y(72) | ATTR0_ROTSCALE | ATTR0_SQUARE;
+    me->attr1 = OBJ_X(112) | ATTR1_SIZE_16 | ATTR1_ROTDATA(affine_ix);
+    for (int i = 1; i < 8; i++)
+    {
+        struct kart* k = &karts[i];
+        s32 x, y;
+        x = ((k->x - me_kart.x) >> 8) + 118;
+        y = ((k->y - me_kart.y) >> 8) + 72;
+
+        if (x < 0 || x > SCREEN_WIDTH || y < 0 || y > SCREEN_HEIGHT)
+            continue;
+        
         OBJATTR* obj = give_shoam_slot();
-        ShoamAffinePtr affine_ptr;
-        u32 affine_ix = give_shoam_affine_slot(&affine_ptr);
-        obj->attr0 = OBJ_Y((k->y >> 8) - 8) | ATTR0_ROTSCALE | ATTR0_SQUARE;
-        obj->attr1 = OBJ_X((k->x >> 8) - 8) | ATTR1_SIZE_16 | ATTR1_ROTDATA(affine_ix);
+        affine_ix = give_shoam_affine_slot(&affine_ptr);
+        obj->attr0 = OBJ_Y(y - 8) | ATTR0_ROTSCALE | ATTR0_SQUARE;
+        obj->attr1 = OBJ_X(x - 8) | ATTR1_SIZE_16 | ATTR1_ROTDATA(affine_ix);
         obj->attr2 = ATTR2_PALETTE(i);
-        ObjAffineSource affine_source = { F8_8(1.0), F8_8(1.0), k->theta };
+        affine_source.theta = k->theta ;
         ObjAffineSet(&affine_source, affine_ptr, 1, 8);
     }
+}
+
+void game(void)
+{
+    clear_shoam();
+
+    keys = ~REG_KEYINPUT;
+
+    if (keys & KEY_LEFT)
+        karts[0].theta_vel = F8_8(5.0);
+    else if (keys & KEY_RIGHT)
+        karts[0].theta_vel = F8_8(-5.0);
+    else 
+        karts[0].theta_vel = 0;
+
+    if (keys & KEY_A)
+        karts[0].vel = F24_8(5.0);
+    
+    random_karts();
+    update_karts();
+    render_karts();
+
 }
 
 void vblank(void)
